c454caef9ecca2b976c0d6c2c35f2e974cb41bce
Eric Thiffeault <ethiffeault@panachedigitalgames.com>
2022-05-25 16:31:22 -0400

    bump version

--- python/etsm/setup.py
+++ python/etsm/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="etsm",                            
-    version="0.2.1",                        
+    version="0.2.2",                        
     author="Eric Thiffeault",               
     description="Efficient Tiny State Machine using object callbacks.",
     long_description=long_description,      
